ea9926094f85cbc3b1e7d3276d517288537ebe98
Levi Njoroge <levynjoroge1@gmail.com>
2025-12-11 12:03:01 +0300

    working on JWA

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -3,7 +3,7 @@ function ChatBot(params) {
     
     return(
         <>
-        
+            <link rel="stylesheet" href="https://app.livechatai.com/ai-bot/cmj16s98x0003la04nknd6auw" />
         </>
     );
 }
